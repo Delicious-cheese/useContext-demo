6de3d4d19f0f3f8627895287f515101e4d8243a3
好喝的芝士 <llric_zhao@163.com>
2022-11-10 15:11:41 +0800

    add style

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -10,11 +10,23 @@ const MovieList = () => {
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'flex-start',
-                gap: 2
+                gap: 2,
+                width: '300px'
             }}
         >
             {
-                movies.map(movie => <li key={movie.id}>{movie.name}</li>)
+                movies.map(movie => (
+                    <li key={movie.id}
+                        style={{
+                            width: '100%',
+                            display: 'flex',
+                            justifyContent: 'space-between'
+                        }}
+                    >
+                        <strong className='text-hide' style={{ width: '56%', textAlign: 'left' }}>{movie.name}</strong>
+                        <span style={{ width: '24%', textAlign: 'left' }}>票价:{movie.price}</span>
+                    </li>
+                ))
             }
         </div>
     )
